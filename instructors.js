685c2c2c1ca1a7c95ba976748b27866142ae86d9
lucaslomba <lucasazevedolomba@gmail.com>
2020-08-15 10:52:49 -0300

    salvando dados e escrevedendo JSON com JS

--- instructors.js
+++ instructors.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const data = require('./data.json')
+
 //Create
 exports.post = function(req, res){
     //req.query
@@ -11,7 +14,16 @@ exports.post = function(req, res){
         }
     }
 
-    return res.send(req.body)
+    req.body.birth = Date.parse(req.body.birth)
+    req.body.created_at = Date.now()
+    //write Json archive for datas
+    data.instructors.push(req.body)
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send("write file error")
+
+        return res.redirect("instructors")
+    })
 }
 
 
