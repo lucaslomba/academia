93246a30c901df2580468c5f84486688571028a5
lucaslomba <lucasazevedolomba@gmail.com>
2020-08-18 10:17:59 -0300

    Formatando data

--- instructors.js
+++ instructors.js
@@ -51,11 +51,23 @@ exports.show = function(req, res){
         return res.send("Instructor not found!")
     }
 
+    function age(timestamp){
+        const today = new Date()
+        const birthDate = new Date(timestamp)
+
+        let age = today.getFullYear() - birthDate.getFullYear()
+
+        const month = today.getMonth() - birthDate.getMonth()
+
+        if(month < 0 || month == 0 ){
+            age = age - 1
+        }
+    }
+
     const instructor = {
         ...foundInstructor,
-        age: "",
-        gender: "",
-        services: "",
+        age: age(foundInstructor.birth),
+        services: foundInstructor.services.split(","),
         created_at: "",
     }
 
