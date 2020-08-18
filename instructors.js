998b3c7e10d6466b69bbe447fd1c43813556e9c8
lucaslomba <lucasazevedolomba@gmail.com>
2020-08-18 10:36:56 -0300

    Formatando data parte 2

--- instructors.js
+++ instructors.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const data = require('./data.json')
+const { age } = require('./utils')
 
 //Create
 exports.post = function(req, res){
@@ -51,24 +52,11 @@ exports.show = function(req, res){
         return res.send("Instructor not found!")
     }
 
-    function age(timestamp){
-        const today = new Date()
-        const birthDate = new Date(timestamp)
-
-        let age = today.getFullYear() - birthDate.getFullYear()
-
-        const month = today.getMonth() - birthDate.getMonth()
-
-        if(month < 0 || month == 0 ){
-            age = age - 1
-        }
-    }
-
     const instructor = {
         ...foundInstructor,
         age: age(foundInstructor.birth),
         services: foundInstructor.services.split(","),
-        created_at: "",
+        created_at: new Intl.DateTimeFormat('pt-BR').format(foundInstructor.created_at),
     }
 
     return res.render("instructors/show", {instructor})
