cb3f92309b0b717a1439a2fc78d2b47ea23b1056
lucaslomba <lucasazevedolomba@gmail.com>
2020-08-18 22:31:50 -0300

    cadastro de membros

--- controllers/members.js
+++ controllers/members.js
@@ -20,21 +20,20 @@ exports.post = function(req, res){
         }
     }
 
-    let {avatar_url, birth, name, services, gender} = req.body
+    birth = Date.parse(req.body.birth)
 
-    birth = Date.parse(birth)
-    const created_at = Date.now()
-    const id = Number(data.members.length + 1)
+    let id = 1
+    const lastMember = data.members[data.members.length - 1]
+
+    if(lastMember){
+        id = lastMember.id + 1
+    }
 
     //write Json archive for datas
     data.members.push({
         id,
-        avatar_url,
-        name,
+        ...req.body,
         birth,
-        gender,
-        services,
-        created_at
     })
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
