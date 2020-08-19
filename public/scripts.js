18367afbe0eb65b032f1a945bb0b5fc89a71dd5f
lucaslomba <lucasazevedolomba@gmail.com>
2020-08-18 21:36:43 -0300

    Menu ativo

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,9 @@
 const currentPage = location.pathname
 
-console.log(currentPage)
+const menuItems = document.querySelectorAll("header .links a")
+
+for (item of menuItems){
+    if(currentPage.includes(item.getAttribute("href"))){
+        item.classList.add("active")
+    }
+}
